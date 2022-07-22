fa1b3933ad9178ebb4677afc57b955c6cadf49d0
MarcoBorgess <marco.borges@branef.com.br>
2022-07-22 13:36:59 -0300

    desbuga ai

--- pages/3_🎮_Hypixel.py
+++ pages/3_🎮_Hypixel.py
@@ -1,3 +1,4 @@
+from calendar import c
 import controller.globalController as globalController
 import controller.itemsController as itemsController
 import streamlit as st
@@ -9,7 +10,11 @@ rows = itemsController.getAllItems()
 text = '# All items:\n'
 
 # Print results.
+count = 0
 for row in rows:
+    count+=1
     text += f"""<img src="{row[6]}" style="height: 16px;"> {row[3]} <br>"""
+    if count > 10:
+        break;
 
 st.markdown(text, unsafe_allow_html=True)
